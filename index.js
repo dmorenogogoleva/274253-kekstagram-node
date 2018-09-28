06e47dc1b2dc1fb09b873b1f838de48ef97ac7a8
Daria Moreno-Gogoleva <iwanttobealight@yandex.ru>
2018-09-28 07:47:12 +0300

    Исправляет ошибки линтера

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const handleUserCommands = cmd => {
+const handleUserCommands = (cmd) => {
   switch (cmd) {
     case `--version`:
-      console.log('v0.0.1');
+      console.log(`v0.0.1`);
       process.exit(0);
       break;
     case `--help`:
-      console.log('Доступные команды: \n --help — печатает этот текст; \n --version — печатает версию приложения;');
+      console.log(`Доступные команды: \n --help — печатает этот текст; \n --version — печатает версию приложения;`);
       process.exit(0);
       break;
     case undefined:
-      console.log('Привет! Эта программа будет запускать сервер кекстаграма');
+      console.log(`Привет! Эта программа будет запускать сервер кекстаграма`);
       process.exit(0);
       break;
     default:
-      console.error(`Неизвестная команда ${cmd}. \n Чтобы прочитать правила использования приложения, наберите "--help"`)
+      console.error(`Неизвестная команда ${cmd}. \n Чтобы прочитать правила использования приложения, наберите "--help"`);
       process.exit(1);
       break;
   }
