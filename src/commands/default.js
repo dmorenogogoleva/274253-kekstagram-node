6552c260682a3549ebc46d0648003f4153afee84
Daria Moreno-Gogoleva <iwanttobealight@yandex.ru>
2018-09-30 21:48:57 +0300

    Раскрашивает текст вывода команды --default

--- src/commands/default.js
+++ src/commands/default.js
@@ -1,7 +1,9 @@
+require(`colors`);
+
 module.exports = {
   name: `default`,
   description: `Shows default message`,
   execute() {
-    console.log(`Привет! Эта программа будет запускать сервер кекстаграма`);
+    console.log(`Привет! Эта программа будет запускать сервер кекстаграма`.cyan);
   }
 };
