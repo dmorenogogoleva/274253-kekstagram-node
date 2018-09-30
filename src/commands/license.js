f64382e83b6b480a729e95d3fa2c3033634f5f78
Daria Moreno-Gogoleva <iwanttobealight@yandex.ru>
2018-09-30 21:51:25 +0300

    Раскрашивает текст вывода команды --license

--- src/commands/license.js
+++ src/commands/license.js
@@ -1,9 +1,10 @@
+require(`colors`);
 const packageInfo = require(`../../package.json`);
 
 module.exports = {
   name: `license`,
   description: `Shows program license`,
   execute() {
-    console.log(packageInfo.license);
+    console.log(packageInfo.license.magenta);
   }
 };
