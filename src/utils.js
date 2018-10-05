7e953cf319401674d4f671c14b5ecb996b497776
Daria Moreno-Gogoleva <iwanttobealight@yandex.ru>
2018-10-05 09:24:16 +0300

    Добавляет utils.js

---
+++ src/utils.js
@@ -0,0 +1,8 @@
+module.exports = {
+  REGEXP_URL: /^(https?:\/\/)?([\w\.]+)\.([a-z]{2,6}\.?)(\/[\w\.]*)*\/?$/,
+  REGEXP_HASHTAG: /^#[а-яa-z0-9]{1,20}$/i,
+  effects: [`none`, `chrome`, `sepia`, `marvin`, `phobos`, `heat`],
+  lengthOfRightHashtagsArr(array) {
+    return array.filter((hashtag) => this.REGEXP_HASHTAG.test(hashtag) ? true : false).length;
+  },
+};
