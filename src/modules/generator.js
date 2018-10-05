3548dd3acfe05e7f9329939872f3fbd0a816b3a4
Daria Moreno-Gogoleva <iwanttobealight@yandex.ru>
2018-10-05 09:24:04 +0300

    Обновляет generator и generator.test

--- src/modules/generator.js
+++ src/modules/generator.js
@@ -3,7 +3,14 @@ module.exports = {
   description: `Generates mock json`,
   generateEntity() {
     return {
-      url: `ememem`,
+      url: `http://placecorgi.com/600/300`,
+      scale: 1,
+      effect: `none`,
+      hashtags: [`#hashtag1`, `#hashtag2`],
+      description: `это больничный мистер черепах Генрих с самыми мерзкими ноздрями эвэр`,
+      likes: 99,
+      comments: [`memee`],
+      date: 1234,
     };
   }
 };
