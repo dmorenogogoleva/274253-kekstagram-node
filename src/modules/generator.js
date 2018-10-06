e370a7bc2ad0a74ad45c789ca2c6711ae7743438
Daria Moreno-Gogoleva <iwanttobealight@yandex.ru>
2018-10-06 15:21:18 +0300

    Меняет данные, которые возвращает функция generateEntity со статических на динамические

--- src/modules/generator.js
+++ src/modules/generator.js
@@ -1,16 +1,55 @@
+const crypto = require(`crypto`);
+const utils = require(`../utils`);
+
+const {effects} = utils;
+
 module.exports = {
   name: `generator`,
   description: `Generates mock json`,
+  descriptionMaxLength: 140,
+  commentMaxLength: 140,
+  hashtagMaxLength: 20,
+  hashtagsArrayMaxLength: 5,
+  getRandomNum(min, max) {
+    return Math.abs(Math.round(min - 0.5 + Math.random() * (max - min + 1)));
+  },
+  getRandomString(length) {
+    return length < 2
+      ? crypto.randomBytes(1).toString(`hex`)
+      : crypto.randomBytes(length / 2).toString(`hex`);
+  },
+  getCommentsArray() {
+    const arr = [];
+    for (let i = 0; i < this.getRandomNum(0, 5); i++) {
+      arr.push(this.getRandomString(this.getRandomNum(1, this.commentMaxLength)));
+    }
+    return arr;
+  },
+  getHashtagsArray() {
+    const arr = [];
+    for (let i = 0; i < this.getRandomNum(0, this.hashtagsArrayMaxLength); i++) {
+      arr.push(`#${this.getRandomString(this.getRandomNum(1, this.hashtagMaxLength))}`);
+    }
+    return arr;
+  },
   generateEntity() {
+    const url = `http://placecorgi.com/600/${this.getRandomNum(200, 800)}`;
+    const scale = this.getRandomNum(0, 100);
+    const effect = effects[this.getRandomNum(0, effects.length - 1)];
+    const hashtags = this.getHashtagsArray();
+    const description = this.getRandomString(this.getRandomNum(1, this.descriptionMaxLength));
+    const likes = this.getRandomNum(0, 1000);
+    const comments = this.getCommentsArray();
+    const date = this.getRandomNum(Date.now() - utils.millisecondsInWeek, Date.now());
     return {
-      url: `http://placecorgi.com/600/300`,
-      scale: 1,
-      effect: `none`,
-      hashtags: [`#hashtag1`, `#hashtag2`],
-      description: `это больничный мистер черепах Генрих с самыми мерзкими ноздрями эвэр`,
-      likes: 99,
-      comments: [`comment1`, `comment2`],
-      date: 1538767371423,
+      url,
+      scale,
+      effect,
+      hashtags,
+      description,
+      likes,
+      comments,
+      date,
     };
   }
 };
--- src/test/generator.test.js
+++ src/test/generator.test.js
@@ -2,100 +2,100 @@ const generator = require(`../modules/generator`);
 const assert = require(`assert`);
 const utils = require(`../utils`);
 
-const generateEntity = generator.generateEntity;
+const generateEntity = generator.generateEntity();
 
 describe(`generateEntity`, () => {
   it(`func have field url`, () => {
-    assert.ok(generateEntity().url, true);
+    assert.ok(generateEntity.url, true);
   });
   it(`field url is a string`, () => {
-    assert.strictEqual(typeof generateEntity().url, `string`);
+    assert.strictEqual(typeof generateEntity.url, `string`);
   });
   it(`url have right format`, () => {
-    assert.ok(utils.REGEXP_URL.test(generateEntity().url), true);
+    assert.ok(utils.REGEXP_URL.test(generateEntity.url), true);
   });
 
 
   it(`func have field scale`, () => {
-    assert.ok(generateEntity().scale, true);
+    assert.ok(generateEntity.scale, true);
   });
   it(`field scale is a number`, () => {
-    assert.strictEqual(typeof generateEntity().scale, `number`);
+    assert.strictEqual(typeof generateEntity.scale, `number`);
   });
   it(`scale is greater than 0`, () => {
-    assert.strictEqual(generateEntity().scale > 0, true);
+    assert.strictEqual(generateEntity.scale > 0, true);
   });
   it(`scale is less than 100`, () => {
-    assert.strictEqual(generateEntity().scale < 100, true);
+    assert.strictEqual(generateEntity.scale < 100, true);
   });
 
   it(`func have field effect`, () => {
-    assert.ok(generateEntity().effect, true);
+    assert.ok(generateEntity.effect, true);
   });
   it(`field effect is a string`, () => {
-    assert.strictEqual(typeof generateEntity().effect, `string`);
+    assert.strictEqual(typeof generateEntity.effect, `string`);
   });
   it(`field effect is one of effects`, () => {
-    assert.ok(utils.effects.includes(generateEntity().effect), true);
+    assert.ok(utils.effects.includes(generateEntity.effect), true);
   });
 
   it(`func have field hashtags`, () => {
-    assert.ok(generateEntity().hashtags, true);
+    assert.ok(generateEntity.hashtags, true);
   });
   it(`field hashtags is an array`, () => {
-    assert.strictEqual(typeof generateEntity().hashtags, `object`);
+    assert.strictEqual(typeof generateEntity.hashtags, `object`);
   });
   it(`field hashtags have less than 5 hashtags`, () => {
-    assert.strictEqual(generateEntity().hashtags.length <= 4, true);
+    assert.strictEqual(generateEntity.hashtags.length <= 4, true);
   });
   it(`hashtags items have right format`, () => {
-    assert.ok(utils.lengthOfRightHashtagsArr(generateEntity().hashtags) === generateEntity().hashtags.length, true);
+    assert.ok(utils.lengthOfRightHashtagsArr(generateEntity.hashtags) === generateEntity.hashtags.length, true);
   });
 
   it(`func have field description`, () => {
-    assert.ok(generateEntity().description, true);
+    assert.ok(generateEntity.description, true);
   });
   it(`field description is a string`, () => {
-    assert.strictEqual(typeof generateEntity().description, `string`);
+    assert.strictEqual(typeof generateEntity.description, `string`);
   });
   it(`description length is less than 140ch`, () => {
-    assert.strictEqual(generateEntity().description.length < 140, true);
+    assert.strictEqual(generateEntity.description.length < 140, true);
   });
 
 
   it(`func have field likes`, () => {
-    assert.ok(generateEntity().likes, true);
+    assert.ok(generateEntity.likes, true);
   });
   it(`field likes is a number`, () => {
-    assert.strictEqual(typeof generateEntity().likes, `number`);
+    assert.strictEqual(typeof generateEntity.likes, `number`);
   });
   it(`likes is greater than 0`, () => {
-    assert.strictEqual(generateEntity().likes > 0, true);
+    assert.strictEqual(generateEntity.likes > 0, true);
   });
   it(`likes is less than 1000`, () => {
-    assert.strictEqual(generateEntity().likes < 1000, true);
+    assert.strictEqual(generateEntity.likes < 1000, true);
   });
 
   it(`func have field comments`, () => {
-    assert.ok(generateEntity().comments, true);
+    assert.ok(generateEntity.comments, true);
   });
   it(`field comments is an array`, () => {
-    assert.strictEqual(typeof generateEntity().comments, `object`);
+    assert.strictEqual(typeof generateEntity.comments, `object`);
   });
   it(`comments items have right format`, () => {
-    assert.ok(utils.lengthOfRightCommentsArr(generateEntity().comments) === generateEntity().comments.length, true);
+    assert.ok(utils.lengthOfRightCommentsArr(generateEntity.comments) === generateEntity.comments.length, true);
   });
 
 
   it(`func have field date`, () => {
-    assert.ok(generateEntity().date, true);
+    assert.ok(generateEntity.date, true);
   });
   it(`field date is an number`, () => {
-    assert.strictEqual(typeof generateEntity().date, `number`);
+    assert.strictEqual(typeof generateEntity.date, `number`);
   });
   it(`field date have right format`, () => {
-    assert.ok(generateEntity().date <= Date.now(), true);
-    assert.ok(generateEntity().date >= Date.now() - utils.millisecondsInWeek, true);
+    assert.ok(generateEntity.date <= Date.now(), true);
+    assert.ok(generateEntity.date >= Date.now() - utils.millisecondsInWeek, true);
   });
 });
 
