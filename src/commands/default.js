d4cedd293a0a74fb9fc662cf9bad13b324aba4a4
Daria Moreno-Gogoleva <iwanttobealight@yandex.ru>
2018-10-08 09:08:27 +0300

    Рефакторинг

--- src/commands/default.js
+++ src/commands/default.js
@@ -6,50 +6,56 @@ require(`colors`);
 
 const writeToFile = promisify(fs.writeFile);
 
-/* eslint-disable max-nested-callbacks */
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const generateData = () => {
+  rl.question(`Сколько элементов хочешь создать? `.cyan, (answer) => {
+    const data = generator.generateEntity(answer);
+
+    rl.question(`Укажи путь до файла в котором ты хочешь сохранить данные `.cyan, (path) => {
+      fs.readFile(path, `utf-8`, (err, file) => {
+        if (err) {
+          writeToFile(path, JSON.stringify(data));
+          console.log(`Данные записаны в файл ${path}`);
+          rl.close();
+        }
+        if (file) {
+          overwriteFile(path, data);
+        }
+      });
+    });
+  });
+};
+
+const overwriteFile = (path, data) => {
+  rl.question(`Такой файл уже существует. Перезаписать? `.cyan, (answer) => {
+    switch (answer) {
+      case `yes`:
+        writeToFile(path, JSON.stringify(data));
+        console.log(`Перезаписан файл ${path}`);
+        break;
+      case `no`:
+        console.log(`Не перезаписывать файл`);
+        break;
+      default:
+        console.log(`Нет такой команды ${answer}`);
+        break;
+    }
+    rl.close();
+  });
+};
 
 module.exports = {
   name: `default`,
   description: `Shows default message`,
   execute() {
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout
-    });
-
     rl.question(`Привет! Хочешь сгенерировать данные?))00) `.cyan, (answer) => {
       switch (answer.toLowerCase()) {
         case `yes`:
-          rl.question(`Сколько элементов хочешь создать? `.cyan, (ans) => {
-            const data = generator.generateEntity(ans);
-            rl.question(`Укажи путь до файла в котором ты хочешь сохранить данные `.cyan, (path) => {
-              fs.readFile(path, `utf-8`, (err, file) => {
-                if (err) {
-                  writeToFile(path, JSON.stringify(data));
-                  rl.close();
-                }
-                if (file) {
-                  rl.question(`Такой файл уже существует. Перезаписать? `.cyan, (answ) => {
-                    switch (answ) {
-                      case `yes`:
-                        writeToFile(path, JSON.stringify(data));
-                        console.log(`Перезаписан файл ${path}`);
-                        rl.close();
-                        break;
-                      case `no`:
-                        console.log(`Не перезаписывать файл`);
-                        rl.close();
-                        break;
-                      default:
-                        console.log(`Нет такой команды`);
-                        rl.close();
-                        break;
-                    }
-                  });
-                }
-              });
-            });
-          });
+          generateData();
           break;
         case `no`:
           console.log(`Очень жаль`);
