6afc03715343d0f6e609586c1adbe94e16fc056a
iwanttobealight <iwanttobealight@yandex.ru>
2018-10-24 08:00:10 +0300

    Исправляет валидацию

--- src/posts/validation.js
+++ src/posts/validation.js
@@ -1,12 +1,29 @@
 const IllegalArgumentError = require(`../errors/illegal-argument-error`);
+const utils = require(`../utils`);
+const {checkHastagsField, checkScaleField, checkDescriptionField} = utils;
 
 const validate = (data) => {
   const errors = [];
-  if (!data.date) {
-    errors.push(`Field 'date' is required`);
+  if (!data.image) {
+    errors.push(`Field 'filename' is required. It should be image formatted jpg || png`);
+  }
+  if (!data.scale) {
+    errors.push(`Field 'scale' is required`);
+  }
+  if (checkScaleField(data.scale)) {
+    errors.push(`Field 'scale' should be a number 1 < 100`);
+  }
+  if (!data.effect) {
+    errors.push(`Field 'effect' is required`);
+  }
+  if (data.hashtags !== undefined && checkHastagsField(data.hashtags)) {
+    errors.push(`Fields '${data.hashtags}' should started with # and be less than 20ch`);
+  }
+  if (data.description !== undefined && checkDescriptionField(data.description)) {
+    errors.push(`Field 'description' should be less than 140ch`);
   }
   if (errors.length > 0) {
-    throw new IllegalArgumentError(errors);
+    throw new IllegalArgumentError(errors.join(`\n`));
   }
   return data;
 };
