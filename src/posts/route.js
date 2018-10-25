680702b9378846760b769b0d0847bbd2e65cf369
iwanttobealight <iwanttobealight@yandex.ru>
2018-10-25 08:46:19 +0300

    Обновляет error handler

--- src/posts/route.js
+++ src/posts/route.js
@@ -116,8 +116,9 @@ const ERROR_HANDLER = (err, req, res, _next) => {
     return;
   } else if (err instanceof NotFoundError) {
     res.status(404).json(err.message);
+  } else {
+    res.status(err.code || 500).send(err.message);
   }
-  res.status(err.code || 500).send(err.message);
 };
 
 postsRouter.use(ERROR_HANDLER);
