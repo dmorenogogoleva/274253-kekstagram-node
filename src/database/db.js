53202e02816d296c0d4a4ed7383c77be5c4188dc
iwanttobealight <iwanttobealight@yandex.ru>
2018-10-26 08:36:46 +0300

    Добавляет логгер

--- src/database/db.js
+++ src/database/db.js
@@ -1,8 +1,11 @@
 const {MongoClient} = require(`mongodb`);
+const logger = require(`../logger`);
 
-const url = `mongodb://localhost:27017`;
+const {DB_HOST, DB_PATH} = process.env;
 
-module.exports = MongoClient.connect(url, {useNewUrlParser: true}).then((client) => client.db(`posts`)).catch((e) => {
-  console.error(`Failed to connect to MongoDB`, e);
+const url = `mongodb://${DB_HOST}`;
+
+module.exports = MongoClient.connect(url, {useNewUrlParser: true}).then((client) => client.db(DB_PATH)).catch((e) => {
+  logger.error(`Failed to connect to MongoDB`, e);
   process.exit(1);
 });
